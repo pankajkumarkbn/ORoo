b80859e5ebcbf639487d0f449f145c601fd5b0b2
pankajkumarkbn <pankajkumarkbn@gmail.com>
2018-03-04 00:52:56 +0530

    Fixed the initial screen
    Signed-off-by: pankajkumarkbn <pankajkumarkbn@gmail.com>

--- app/src/main/java/com/example/dd/oroo/MainActivity.java
+++ app/src/main/java/com/example/dd/oroo/MainActivity.java
@@ -22,45 +22,45 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "Tap Again", Toast.LENGTH_SHORT).show();
 
-                if (count == 1) {
+                if (count == 0) {
                     linearLayout.setBackgroundColor(getResources().getColor(R.color.whitu));
                     textView.setTextColor(getResources().getColor(R.color.blacku));
                     textView.setText("People inspire you or drain you; pick them wisely.");
                 }
-                if (count == 2) {
+                if (count == 1) {
                     linearLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
                     textView.setTextColor(getResources().getColor(R.color.whitu));
                     textView.setText("By appreciation, we make excellence in others our own property.");
                 }
-                if (count == 3) {
+                if (count == 2) {
                     linearLayout.setBackgroundColor(getResources().getColor(R.color.colorAccent));
                     textView.setTextColor(getResources().getColor(R.color.whitu));
                     textView.setText("One of the truest tests of integrity is that its blunt refusal to be compromised.");
                 }
-                if (count == 4) {
+                if (count == 3) {
                     linearLayout.setBackgroundColor(getResources().getColor(R.color.blacku));
                     textView.setText("Hate no one; hate their vices, not themselves.");
 
                 }
-                if (count == 5) {
+                if (count == 4) {
                     linearLayout.setBackgroundColor(getResources().getColor(R.color.whitu));
                     textView.setTextColor(getResources().getColor(R.color.blacku));
                     textView.setText("Stop wearing your wishbone where your backbone ought to be.");
                 }
-                if (count == 6) {
+                if (count == 5) {
                     linearLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
                     textView.setTextColor(getResources().getColor(R.color.whitu));
                     textView.setText("Truth can be stated in a thousand different ways, yet each one can be true.");
                 }
-                if (count == 7) {
+                if (count == 6) {
                     linearLayout.setBackgroundColor(getResources().getColor(R.color.colorAccent));
                     textView.setTextColor(getResources().getColor(R.color.whitu));
                     textView.setText("All who would win joy, must share it; happiness was born a twin.");
                 }
 
                 count++;
-                if (count > 7) {
-                    count = 1;
+                if (count > 6) {
+                    count = 0;
                 }
             }
         });
